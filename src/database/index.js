57279a4e374425c329804149a2dda7c90ca1a05c
eduhirt <ehirt@inf.ufsm.br>
2018-09-04 22:38:47 -0300

    trycatch error

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://eduardohirt@hotmail.com:eduardo031096.@https://ps2-show-manager.herokuapp.com/eventop:3000', { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+
+
+try{
+    mongoose.Promise = global.Promise;
+} catch(err){
+    throw('Erro mongodb', err)
+}
+
 
 module.exports = mongoose;
