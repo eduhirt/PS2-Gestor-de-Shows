21c4e9706392e67c3634bb1774e6e17e5c103d9c
eduhirt <ehirt@inf.ufsm.br>
2018-09-04 22:28:16 -0300

    mongo url + config file

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://https://ps2-show-manager.herokuapp.com/eventop', { useNewUrlParser: true });
+mongoose.connect('mongodb://eduardohirt@hotmail.com:eduardo031096.@https://ps2-show-manager.herokuapp.com/eventop', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
